8b747e94f60570b1b7d6b0ca132537a6dfd96cba
Carlos Romero <carlos_ederh@hotmail.com>
2019-05-31 22:43:21 -0600

    Modificar usuarios

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -8,15 +8,15 @@ class Users extends MainController
 
     /**
      * index
-     * 
+     *
      * Método principal del controlador usuarios
-     * 
+     *
      * Carga la vista principal del modulo usuarios
      * Guarda la recibe y enviá al modelo la información de los nuevos administrativos
      * usuarios que se registran al sistema
      *
      * @param string $alert , identifica si se debe mostrar un sweet alert
-     * 
+     *
      * @author Wilber Méndez
      */
     public function index($alert = ''){
@@ -34,7 +34,7 @@ class Users extends MainController
             // encrestamos la contraseña utilizando el algoritmo sha512 agregando un SALT para mas sefuridad
             $user['password'] = hash('sha512', SALT . sanitize($_POST['password']));
 
-            
+
             if ($this->ModelUsers->add_user($user)) {
                 redirect('/users/saved');
             } else {
@@ -57,12 +57,12 @@ class Users extends MainController
 
     /**
      * disable
-     * 
+     *
      * Función para desactivar usuarios
-     * 
+     *
      * Esta función cambia el estado de un usuario a desactivado
      * recibiendo como parámetro su id de esta forma el usuario
-     * no podrá iniciar sesión de nuevo hasta que lo vuelvan a 
+     * no podrá iniciar sesión de nuevo hasta que lo vuelvan a
      * activar
      *
      * @param [type] $id
@@ -80,7 +80,40 @@ class Users extends MainController
         ];
         $this->view('users/disable', $parameters);
     }
-    
+
+    public function update($id = 0)
+    {
+      if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+          // Limpiamos los datos para prevenir inyección de código
+          $user['name'] = sanitize($_POST['first_name']);
+          $user['last_name'] = sanitize($_POST['last_name']);
+          $user['birthdate'] = sanitize($_POST['birthdate']);
+          $user['gender'] = sanitize($_POST['gender']);
+          $user['user_type'] = sanitize($_POST['user_type']);
+          $user['email'] = sanitize($_POST['email']);
+
+          if ($this->ModelUsers->update_user($id, $user)) {
+              header("location:".ROUTE_URL."/users/update/".$id);
+              //redirect('/users/saved');
+          } else {
+              die('Error al guardar los datos');
+          }
+      }
+
+      $user = $this->ModelUsers->get_user($id);
+      if (!$user) {
+        header('location:'.ROUTE_URL.'/users');
+      }
+
+      $parameters = [
+        'menu' => 'users',
+        'user' => $user
+      ];
+
+      $this->view('users/update', $parameters);
+    }
+
 }
 
 
