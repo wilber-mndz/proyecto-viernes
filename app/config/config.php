ffe865ee40d0128561fed5029b6b62e2b7069ceb
Carlos Romero <carlos_ederh@hotmail.com>
2019-06-09 20:06:15 -0600

    Actualizando Repo

--- app/config/config.php
+++ app/config/config.php
@@ -1,14 +1,14 @@
 <?php
     // Credenciales de acceso a la base de datos
-    define('DB_HOST', 'SOUL:5000');
-    define('DB_USER', 'sa');
-    define('DB_NAME', 'dbfriday');
-    define('DB_PASS', '123456');
-
-    // define('DB_HOST', 'CEDERH:5000');
+    // define('DB_HOST', 'SOUL:5000');
     // define('DB_USER', 'sa');
     // define('DB_NAME', 'dbfriday');
-    // define('DB_PASS', 'cederh');
+    // define('DB_PASS', '123456');
+
+    define('DB_HOST', 'CEDERH:5000');
+    define('DB_USER', 'sa');
+    define('DB_NAME', 'dbfriday');
+    define('DB_PASS', 'cederh');
 
     // Establecemos la ruta de nuestra aplicación
     define('ROUTE_APP', dirname(dirname(__FILE__)));
